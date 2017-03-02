cee72d4a0030d4be0881021d79c670712ee6866b
David Yang <dayang@google.com>
2017-03-01 19:01:48 -0800

    in progress
    Change-Id: I41a33893f23f1699619195a85a030fda36c0ad09

---
+++ modules/chat/lib/src/widgets/chat_thread_list.dart
@@ -0,0 +1,71 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import 'chat_thread_list_item.dart';
+
+/// UI Widget that represents a list of chat threads
+class ChatThreadList extends StatelessWidget {
+
+  /// List of [ChatThreadListItems] to render
+  ///
+  /// TODO(dayang): Pass in the data model representing a chat thread
+  /// once that is specified
+  final List<ChatThreadListItem> chatThreads;
+
+  /// Constructor
+  ChatThreadList({
+    Key key,
+    @required this.chatThreads,
+  }) : super(key: key) {
+    assert(this.chatThreads != null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return new Stack(
+      children: <Widget>[
+        new Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new Container(
+              height: 56.0,
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: FractionalOffset.centerLeft,
+              decoration: new BoxDecoration(
+                border: new Border(
+                  bottom: new BorderSide(color: Colors.grey[300])
+                ),
+              ),
+              child: new Text(
+                'Chat',
+                style: theme.textTheme.title,
+              ),
+            ),
+            new ListView(
+              shrinkWrap: true,
+              children: chatThreads,
+            ),
+          ]
+        ),
+        new Positioned(
+          bottom: 16.0,
+          right: 16.0,
+          child: new FloatingActionButton(
+            child: new Icon(Icons.add),
+            onPressed: () {
+              print('pint!');
+            }
+          ),
+        ),
+
+      ],
+    );
+
+  }
+}
